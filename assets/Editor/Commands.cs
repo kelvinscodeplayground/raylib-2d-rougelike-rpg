412a3d3ba3d8c30b79b2eb65a8ce4287afc24a6f
Kelvin Chin <chinkaiwen_35@outlook.com>
2024-06-03 02:19:04 +0800

    Added assets for game dev

---
+++ assets/Editor/Commands.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Completed;
+
+using LunarPlugin;
+
+#region Days Management
+
+[CCommand("restart", Description="Restarts current day")]
+class Cmd_restart : CPlayModeCommand
+{
+	void Execute()
+	{
+		GameManager.instance.RestartDay();
+	}
+}
+
+[CCommand("next", Description="Starts next day")]
+class Cmd_next : CPlayModeCommand
+{
+	void Execute()
+	{
+		GameManager.instance.NextDay();
+	}
+}
+
+[CCommand("prev", Description="Starts prev day")]
+class Cmd_prev : CPlayModeCommand
+{
+	void Execute()
+	{
+		GameManager.instance.PrevDay();
+	}
+}
+
+[CCommand("day", Description="Starts Nth day")]
+class Cmd_day : CPlayModeCommand
+{
+	void Execute(int day)
+	{
+		GameManager.instance.StartDay(day);
+	}
+}
+
+#endregion
+
+#region Food Management
+
+[CCommand("food")]
+class Cmd_food : CPlayModeCommand
+{
+	bool Execute(int food)
+	{
+		Player player = GameObject.FindObjectOfType<Player>();
+		if (player == null)
+		{
+			PrintError("Player not found");
+			return false;
+		}
+		
+		player.Food = food;
+		return true;
+	}
+}
+
+#endregion

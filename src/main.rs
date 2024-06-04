d8e4a656c9f9168f7b03acece37bb0738b5dbe26
Kelvin Chin <chinkaiwen_35@outlook.com>
2024-06-05 02:25:34 +0800

    Setup basic template for the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .title("2D Rougelike Demo")
+        .size(1280, 720)
+        .build();
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        let mut brush = rl.begin_drawing(&thread);
+        brush.clear_background(Color::WHITE);
+    }
 }

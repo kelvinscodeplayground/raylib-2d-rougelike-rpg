dd5e46f094280871e2c87a1e6aec744e6381730b
Kelvin Chin <chinkaiwen_35@outlook.com>
2024-06-08 04:19:48 +0800

    Added raylib-cpp to simplify work

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,47 +1,39 @@
-#include <raylib.h>
 #include <memory>
 #include <functional>
 
-#include "Dto/Font.hpp"
-#include "Dto/Image.hpp"
-#include "Dto/Music.hpp"
+#include <raylib-cpp.hpp>
 
 static constexpr int WindowHeight { 480 };
 static constexpr int WindowWidth { 640 };
 
 int main()
 {
-    InitWindow(WindowWidth, WindowHeight, "2D Rouge Like");
-    SetTargetFPS(60);
-    InitAudioDevice();
-    auto windowHandler =
-            std::unique_ptr<uint8_t, std::function<void(uint8_t *)>>(new uint8_t, [](uint8_t *ptr) {
-                delete ptr;
-                CloseWindow();
-                CloseAudioDevice();
-            });
+    raylib::Window window { WindowWidth, WindowHeight, "2D Rouge Like" };
+    raylib::AudioDevice audio;
+    raylib::Image icon { "assets/raylib_256x256.png" };
+    raylib::Font font { "assets/Fonts/PressStart2P-Regular.ttf" };
 
-    raylib::dto::Image icon { "assets/raylib_256x256.png" };
-    SetWindowIcon(*icon);
+    window.SetTargetFPS(60);
+    window.SetIcon(icon);
 
-    raylib::dto::Font font;
+    // raylib::dto::Music bgm { "assets/Audio/scavengers_music.aif.wav" };
+    // bgm.playMusicStream();
+    raylib::Music bgm { "assets/Audio/scavengers_music.aif.wav" };
+    bgm.Play();
 
-    raylib::dto::Music bgm { "assets/Audio/scavengers_music.aif.wav" };
-    bgm.playMusicStream();
+    while (!window.ShouldClose()) {
+        bgm.Update();
 
-    while (!WindowShouldClose()) {
-        bgm.updateMusicStream();
-
-        BeginDrawing();
+        window.BeginDrawing();
         auto painter = std::unique_ptr<uint8_t, std::function<void(uint8_t *)>> { new uint8_t,
-            [](uint8_t *ptr) {
+            [&window](uint8_t *ptr) {
                 delete ptr;
-                EndDrawing();
+                window.EndDrawing();
             } };
 
-        ClearBackground({ 255, 255, 255, 255 });
+        window.ClearBackground({ 255, 255, 255, 255 });
         // DrawText("Hello World", 15, 15, 32, { 0, 0, 0, 255 });
-        DrawTextEx(font.data(), "Hello World", { 15, 15 }, 32, 0, { 0, 0, 0, 255 });
+        raylib::Color::Black().DrawText(font, "Hello World", { 15, 15 }, 32, 1);
     }
 
     return 0;
